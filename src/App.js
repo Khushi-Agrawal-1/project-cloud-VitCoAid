078585ef40760086273a07262c2426b1eaa0daf2
Khushi Agrawal <khushi.agrawal2019@vitstudent.ac.in>
2021-12-05 03:57:27 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
@@ -10,13 +10,13 @@ Amplify.configure(awsconfig);
 function App() {
     return (
         <div className="App">
-             {/* <Navbar>
+             <Navbar>
                 <Navbar.Header>
                  <Navbar.Brand>
                      VitCoAid
                 </Navbar.Brand>
                 </Navbar.Header>
-             </Navbar> */}
+             </Navbar>
             <header className="App-header">
                 <AmplifySignOut />
                 <h2>My App Content</h2>
